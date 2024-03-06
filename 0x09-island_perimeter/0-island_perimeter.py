7a13820e517afe72a12a74477e9640230f46973a
Koyejo <adinlewakoyejo@yahoo.com>
2024-03-06 07:18:04 -0800

    Add func to compute perimeter of island as 2D grid

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Create a function that computes the perimeter of a given island"""
+from typing import List
+
+def island_perimeter(grid: List[List[int]]) -> int:
+    """
+    Calculate the perimeter of the island in a 2D grid.
+
+    Arg(s):
+        grid(List[List[int]]): A list of lists of integers representing
+                               the grid. 0 reps water, 1 reps land.
+
+    Returns:
+        The perimeter of the island as an int.
+    """
+    rows, cols = len(grid), len(grid[0])
+    perimeter = 0
+
+    # Loop through the grid
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 1:
+                # Check all four neighbors
+                if row - 1 < 0 or grid[row - 1][col] == 0:
+                    perimeter += 1  # Top neighbor is water
+                if row + 1 >= rows or grid[row + 1][col] == 0:
+                    perimeter += 1  # Bottom neighbor is water
+                if col - 1 < 0 or grid[row][col - 1] == 0:
+                    perimeter += 1  # Left neighbor is water
+                if col + 1 >= cols or grid[row][col + 1] == 0:
+                    perimeter += 1  # Right neighbor is water
+    return perimeter
